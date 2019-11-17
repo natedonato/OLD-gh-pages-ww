209fb5912d785154c091a658b1dd7deab2d1d20d
Nate Donato <natedonato@gmail.com>
2019-11-17 12:57:21 -0800

    Updates

---
+++ precache-manifest.7a76bee3ba9b0c2c0f18d162065067d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1ac5b22106df9489c6a3720a6c92b0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bcfae419a99bf3aaa3ad",
+    "url": "/portfolio/static/css/main.b98a36d5.chunk.css"
+  },
+  {
+    "revision": "b82997519663f5b5d285",
+    "url": "/portfolio/static/js/2.fe0149c6.chunk.js"
+  },
+  {
+    "revision": "bcfae419a99bf3aaa3ad",
+    "url": "/portfolio/static/js/main.e28bfba4.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f11c5d0c5b7008f5768309c459e37734",
+    "url": "/portfolio/static/media/testlogo.f11c5d0c.png"
+  }
+]);
